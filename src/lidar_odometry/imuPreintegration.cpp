0218cb9862fff849ed523be197efffc9f4965974
Lihaha-pro <hit_llh@163.com>
2022-10-24 09:25:53 +0800

    学习完预积分部分

--- src/lidar_odometry/imuPreintegration.cpp
+++ src/lidar_odometry/imuPreintegration.cpp
@@ -121,15 +121,20 @@ public:
         doneFirstOpt = false;
         systemInitialized = false;
     }
-
+    /**
+     * @brief 订阅地图优化节点的增量里程计消息
+     * 
+     * @param odomMsg 
+     */
     void odometryHandler(const nav_msgs::Odometry::ConstPtr& odomMsg)
     {
-        double currentCorrectionTime = ROS_TIME(odomMsg);
+        double currentCorrectionTime = ROS_TIME(odomMsg);//取出时间戳
 
         // make sure we have imu data to integrate
+        //确保IMU队列有数据
         if (imuQueOpt.empty())
             return;
-
+        //获得里程计位姿
         float p_x = odomMsg->pose.pose.position.x;
         float p_y = odomMsg->pose.pose.position.y;
         float p_z = odomMsg->pose.pose.position.z;
@@ -138,6 +143,7 @@ public:
         float r_z = odomMsg->pose.pose.orientation.z;
         float r_w = odomMsg->pose.pose.orientation.w;
         int currentResetId = round(odomMsg->pose.covariance[0]);
+        //gtsam格式雷达坐标系下位姿
         gtsam::Pose3 lidarPose = gtsam::Pose3(gtsam::Rot3::Quaternion(r_w, r_x, r_y, r_z), gtsam::Point3(p_x, p_y, p_z));
 
         // correction pose jumped, reset imu pre-integration
@@ -150,11 +156,14 @@ public:
 
 
         // 0. initialize system
+        //第一次运行的话，先初始化系统
         if (systemInitialized == false)
         {
+            //优化问题进行复位
             resetOptimization();
 
             // pop old IMU message
+            //将该帧里程计消息之前的imu信息全部扔掉
             while (!imuQueOpt.empty())
             {
                 if (ROS_TIME(&imuQueOpt.front()) < currentCorrectionTime - delta_t)
@@ -166,32 +175,37 @@ public:
                     break;
             }
             // initial pose
+            //将lidar的位姿转移到IMU坐标系下
             prevPose_ = lidarPose.compose(lidar2Imu);
+            //设置初始位姿置信度
             gtsam::PriorFactor<gtsam::Pose3> priorPose(X(0), prevPose_, priorPoseNoise);
-            graphFactors.add(priorPose);
-            // initial velocity
+            graphFactors.add(priorPose); //约束添加至因子图中
+            // initial velocity 初始化速度，设为0了，置信度不高
             prevVel_ = gtsam::Vector3(0, 0, 0);
             gtsam::PriorFactor<gtsam::Vector3> priorVel(V(0), prevVel_, priorVelNoise);
             graphFactors.add(priorVel);
-            // initial bias
+            // initial bias 初始化零偏，直接用gtsam默认值0
             prevBias_ = gtsam::imuBias::ConstantBias();
             gtsam::PriorFactor<gtsam::imuBias::ConstantBias> priorBias(B(0), prevBias_, priorBiasNoise);
             graphFactors.add(priorBias);
             // add values
+            // 将各个状态量设为初始值
             graphValues.insert(X(0), prevPose_);
             graphValues.insert(V(0), prevVel_);
             graphValues.insert(B(0), prevBias_);
             // optimize once
+            //状态和约束添加进isam优化器
             optimizer.update(graphFactors, graphValues);
+            //进入优化器后，保存状态和约束的临时变量清零
             graphFactors.resize(0);
             graphValues.clear();
-
+            //预积分接口，使用初始化零偏进行初始化
             imuIntegratorImu_->resetIntegrationAndSetBias(prevBias_);
             imuIntegratorOpt_->resetIntegrationAndSetBias(prevBias_);
             
             key = 1;
             systemInitialized = true;
-            return;
+            return;//初始化之后，直接跳出
         }
 
 
@@ -204,9 +218,9 @@ public:
             gtsam::noiseModel::Gaussian::shared_ptr updatedBiasNoise = gtsam::noiseModel::Gaussian::Covariance(optimizer.marginalCovariance(B(key-1)));
             // reset graph
             resetOptimization();
-            // add pose
+            // add pose 设置初始位姿和置信度
             gtsam::PriorFactor<gtsam::Pose3> priorPose(X(0), prevPose_, updatedPoseNoise);
-            graphFactors.add(priorPose);
+            graphFactors.add(priorPose); 
             // add velocity
             gtsam::PriorFactor<gtsam::Vector3> priorVel(V(0), prevVel_, updatedVelNoise);
             graphFactors.add(priorVel);
@@ -227,18 +241,21 @@ public:
 
 
         // 1. integrate imu data and optimize
+        //预积分两帧之间的IMU数据
         while (!imuQueOpt.empty())
         {
             // pop and integrate imu data that is between two optimizations
-            sensor_msgs::Imu *thisImu = &imuQueOpt.front();
+            sensor_msgs::Imu *thisImu = &imuQueOpt.front();//取出IMU数据
             double imuTime = ROS_TIME(thisImu);
             if (imuTime < currentCorrectionTime - delta_t)
             {
+                //计算两帧IMU时间差
                 double dt = (lastImuT_opt < 0) ? (1.0 / 500.0) : (imuTime - lastImuT_opt);
+                //调用预积分接口送入imu数据进行处理
                 imuIntegratorOpt_->integrateMeasurement(
                         gtsam::Vector3(thisImu->linear_acceleration.x, thisImu->linear_acceleration.y, thisImu->linear_acceleration.z),
                         gtsam::Vector3(thisImu->angular_velocity.x,    thisImu->angular_velocity.y,    thisImu->angular_velocity.z), dt);
-                
+                //记录当前帧imu时间戳，用于下次积分
                 lastImuT_opt = imuTime;
                 imuQueOpt.pop_front();
             }
@@ -246,37 +263,48 @@ public:
                 break;
         }
         // add imu factor to graph
+        //两帧间imu预积分完成后，转换成预积分约束
         const gtsam::PreintegratedImuMeasurements& preint_imu = dynamic_cast<const gtsam::PreintegratedImuMeasurements&>(*imuIntegratorOpt_);
+        //预积分约束对相邻两帧之间的位姿 速度 零偏形成约束
         gtsam::ImuFactor imu_factor(X(key - 1), V(key - 1), X(key), V(key), B(key - 1), preint_imu);
+        //加入因子图
         graphFactors.add(imu_factor);
         // add imu bias between factor
+        //对相邻两帧位姿间的零偏进行常量约束，认为零偏不会变化太大
         graphFactors.add(gtsam::BetweenFactor<gtsam::imuBias::ConstantBias>(B(key - 1), B(key), gtsam::imuBias::ConstantBias(),
                          gtsam::noiseModel::Diagonal::Sigmas(sqrt(imuIntegratorOpt_->deltaTij()) * noiseModelBetweenBias)));
         // add pose factor
         gtsam::Pose3 curPose = lidarPose.compose(lidar2Imu);
+        //lidar位姿转换到imu系下，作为这一帧的先验估计
         gtsam::PriorFactor<gtsam::Pose3> pose_factor(X(key), curPose, correctionNoise);
         graphFactors.add(pose_factor);
         // insert predicted values
+        // 根据上一时刻状态，结合预积分结果，对当前状态进行预测（这样可以获得速度，直接用lidar位姿没有速度信息）
         gtsam::NavState propState_ = imuIntegratorOpt_->predict(prevState_, prevBias_);
+        //预测结果作为初始值插入因子图
         graphValues.insert(X(key), propState_.pose());
         graphValues.insert(V(key), propState_.v());
         graphValues.insert(B(key), prevBias_);
-        // optimize
+        // optimize（优化两次）
         optimizer.update(graphFactors, graphValues);
         optimizer.update();
         graphFactors.resize(0);
         graphValues.clear();
         // Overwrite the beginning of the preintegration for the next step.
+        //获得当前时刻最优估计，记录结果
         gtsam::Values result = optimizer.calculateEstimate();
         prevPose_  = result.at<gtsam::Pose3>(X(key));
         prevVel_   = result.at<gtsam::Vector3>(V(key));
         prevState_ = gtsam::NavState(prevPose_, prevVel_);
         prevBias_  = result.at<gtsam::imuBias::ConstantBias>(B(key));
         // Reset the optimization preintegration object.
+        //优化完成，预积分复位，并重设零偏值
         imuIntegratorOpt_->resetIntegrationAndSetBias(prevBias_);
         // check optimization
+        //一个简单的失效检测
         if (failureDetection(prevVel_, prevBias_))
         {
+            //状态异常则复位
             resetParams();
             return;
         }
@@ -317,6 +345,7 @@ public:
     bool failureDetection(const gtsam::Vector3& velCur, const gtsam::imuBias::ConstantBias& biasCur)
     {
         Eigen::Vector3f vel(velCur.x(), velCur.y(), velCur.z());
+        //速度超过30m/s
         if (vel.norm() > 30)
         {
             ROS_WARN("Large velocity, reset IMU-preintegration!");
@@ -325,6 +354,7 @@ public:
 
         Eigen::Vector3f ba(biasCur.accelerometer().x(), biasCur.accelerometer().y(), biasCur.accelerometer().z());
         Eigen::Vector3f bg(biasCur.gyroscope().x(), biasCur.gyroscope().y(), biasCur.gyroscope().z());
+        //零偏太大也认为异常
         if (ba.norm() > 0.1 || bg.norm() > 0.1)
         {
             ROS_WARN("Large bias, reset IMU-preintegration!");
@@ -336,13 +366,13 @@ public:
 
     void imuHandler(const sensor_msgs::Imu::ConstPtr& imuMsg)
     {
-        sensor_msgs::Imu thisImu = imuConverter(*imuMsg);
+        sensor_msgs::Imu thisImu = imuConverter(*imuMsg);//当前IMU数据
         // publish static tf
         tfMap2Odom.sendTransform(tf::StampedTransform(map_to_odom, thisImu.header.stamp, "map", "odom"));
-
+        //一个队列用于预积分和位姿优化，一个用来更新IMU状态
         imuQueOpt.push_back(thisImu);
         imuQueImu.push_back(thisImu);
-
+        //如果没发生过优化，就结束这次循环，退出
         if (doneFirstOpt == false)
             return;
 
